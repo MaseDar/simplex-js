e9ee310d4cf2d2bd9b88ced8085b591bae5c8a56
MaseDar Notebook <mailspecialforspam@mail.ru>
2021-05-13 23:09:00 +0300

    add gauss, but have some problems

--- src/components/methods/simplex/simplexAlgrothm.js
+++ src/components/methods/simplex/simplexAlgrothm.js
@@ -1,7 +1,9 @@
-export default function startSolution(countVariables, func, countRestrictions, restrictions, minMax){
+import Solution from './Gauss';
+
+export default function startSolution(countVariables, func, countRestrictions, restrictions, minMax, basisVariables = [1, 1, 0, 0]){
  
     /*  Пример, чтобы понять что делать дальше
-    * [-1]x1+[-2]x2+[-3]x3+[-4]x4+[-5]x5 -> min/max
+    * [-1]x1+[-2]x2+[-3]x3+[-4]x4+[-5]x5 -> min/max 
     * Строим таблицу и используем метод гаусса для базисных переменных 
     *  x1 x2 x3 x4 x5  b                                                                          беру значение у r[x1] = x4 
     * [1, 0, 0, 2, 3] [8]     выраженные значения пусть будут r[x]                x4  x5  c       (f(x1))* [r[x1]] + ...    
@@ -15,4 +17,12 @@ export default function startSolution(countVariables, func, countRestrictions, r
     * [*, *] [c] <- mylittlepony[]
     */
 
+    // Пока решаем для минимума, т.к. позже можно будет перевести из максимума путем смены знаков.
+
+    // НАДО ФИКСИТЬ РЕШЕНИЕ В ГАУССЕ, А ТО ОСНОВУ НОРМ СЧИТАЕТ, А ответ (=b) нет!
+    Solution(restrictions)
+    let last = restrictions[0].length - 1;
+    console.log("res", restrictions)
+    console.log("res[1][-1]", restrictions[1][last])
+    
 }
